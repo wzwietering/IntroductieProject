e0398438ff22ce59ed6d2a87d69228f161f93fcb
Wilmer Zwietering <wzwietering@gmail.com>
2016-11-29 13:59:12 +0100

    Formatting changes

--- app/src/main/java/com/edulectronics/tinycircuit/ui/CircuitActivity.java
+++ app/src/main/java/com/edulectronics/tinycircuit/ui/CircuitActivity.java
@@ -10,7 +10,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.edulectronics.tinycircuit.Circuit.CircuitController;
 import com.edulectronics.tinycircuit.R;
@@ -21,7 +20,8 @@ public class CircuitActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         GridView circuit = (GridView) findViewById(R.id.circuit);
 
@@ -36,18 +36,21 @@ public class CircuitActivity extends AppCompatActivity {
             }
         });
         Intent intent = getIntent();
-        CircuitController controller = (CircuitController) intent.getSerializableExtra("Controller");
+        CircuitController controller =
+                (CircuitController) intent.getSerializableExtra("Controller");
         circuit.setNumColumns(controller.circuit.width);
         circuit.setAdapter(new CircuitAdapter(this, controller));
 
         /*This code adds a menu to the side*/
         ListView menu = (ListView) findViewById(R.id.menu);
         String[] items = {"Item1", "Item2"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, items);
         menu.setAdapter(adapter);
 
         menu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long itemId){
+            public void onItemClick(AdapterView<?> adapterView, View view,
+                                    int position, long itemId){
                 /*Not implemented, should select desired item*/
             }
         });
