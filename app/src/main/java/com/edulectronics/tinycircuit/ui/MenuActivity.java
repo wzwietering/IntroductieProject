11c6441b9ebfc387bdbb712c0373c184bc824f6e
Wilmer Zwietering <wzwietering@gmail.com>
2016-11-30 09:14:15 +0100

    Merge branch 'bernd/fullscreen'
    # Conflicts:
    #	app/src/main/java/com/edulectronics/tinycircuit/ui/MainActivity.java

--- app/src/main/java/com/edulectronics/tinycircuit/ui/MenuActivity.java
+++ app/src/main/java/com/edulectronics/tinycircuit/ui/MenuActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.edulectronics.tinycircuit.Circuit.CircuitController;
@@ -14,6 +15,8 @@ public class MenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_menu);
 
         Button startGameButton = (Button) findViewById(R.id.startGame);
