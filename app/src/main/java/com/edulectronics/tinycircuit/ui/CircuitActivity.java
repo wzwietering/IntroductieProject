8a017be8b8da0ffaef6e156b00665aa5545c9cb3
Wilmer Zwietering <wzwietering@gmail.com>
2016-12-01 14:44:36 +0100

    Brand new menu. It's items are now expandable and you can add images to them.

--- app/src/main/java/com/edulectronics/tinycircuit/ui/CircuitActivity.java
+++ app/src/main/java/com/edulectronics/tinycircuit/ui/CircuitActivity.java
@@ -1,22 +1,29 @@
 package com.edulectronics.tinycircuit.ui;
 
 import android.content.Intent;
-import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.GridView;
-import android.widget.ListView;
 
 import com.edulectronics.tinycircuit.Circuit.CircuitController;
+import com.edulectronics.tinycircuit.Models.ExpandedMenu;
 import com.edulectronics.tinycircuit.R;
 import com.edulectronics.tinycircuit.ui.adapters.CircuitAdapter;
+import com.edulectronics.tinycircuit.ui.adapters.ExpandableListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class CircuitActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private List<ExpandedMenu> headers;
+    private HashMap<ExpandedMenu, List<String>> children;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +43,7 @@ public class CircuitActivity extends AppCompatActivity {
                 return false;
             }
         });
+
         Intent intent = getIntent();
         CircuitController controller =
                 (CircuitController) intent.getSerializableExtra("Controller");
@@ -47,17 +55,65 @@ public class CircuitActivity extends AppCompatActivity {
 
     /*This code adds a menu to the side*/
     private void makeMenu(){
-        ListView menu = (ListView) findViewById(R.id.menu);
-        String[] items = getResources().getStringArray(R.array.items);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this, android.R.layout.simple_list_item_1, items);
-        menu.setAdapter(adapter);
+        ExpandableListView expandableList = (ExpandableListView) findViewById(R.id.expandablelist);
+        drawerLayout = (DrawerLayout) findViewById(R.id.activity_main);
+        prepareListData();
 
-        menu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> adapterView, View view,
-                                    int position, long itemId){
-                /*Not implemented, should select desired item*/
+        ExpandableListAdapter adapter = new ExpandableListAdapter(
+                this, headers, children, expandableList);
+        expandableList.setAdapter(adapter);
+
+        expandableList.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView expandableListView, View view,
+                                        int groupPosition, int childPosition, long rowID) {
+                /*Handle click event of child over here*/
+                return false;
             }
         });
+        expandableList.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView expandableListView, View view,
+                                        int groupPosition, long rowID) {
+                /*Handle click event of group over here*/
+                return false;
+            }
+        });
+    }
+
+    /*Makes the groups and children*/
+    private void prepareListData() {
+        headers = new ArrayList<>();
+        children = new HashMap<>();
+        String[] items = getResources().getStringArray(R.array.menuitems);
+
+        ExpandedMenu item1 = new ExpandedMenu();
+        item1.setIconName(items[0]);
+        item1.setIconImage(R.mipmap.ic_launcher);
+        headers.add(item1);
+
+        ExpandedMenu item2 = new ExpandedMenu();
+        item2.setIconName(items[1]);
+        item2.setIconImage(R.mipmap.ic_launcher);
+        headers.add(item2);
+
+        ExpandedMenu item3 = new ExpandedMenu();
+        item3.setIconName(items[2]);
+        item3.setIconImage(R.mipmap.ic_launcher);
+        headers.add(item3);
+
+        /*Add child data*/
+        List<String> heading1 = new ArrayList();
+        heading1.add("Batterij");
+
+        List<String> heading2 = new ArrayList();
+        heading2.add("Gloeilamp");
+
+        List<String> heading3 = new ArrayList();
+        heading3.add("Weerstand");
+
+        children.put(headers.get(0), heading1);
+        children.put(headers.get(1), heading2);
+        children.put(headers.get(2), heading3);
     }
 }
---
+++ app/src/main/java/com/edulectronics/tinycircuit/ui/adapters/ExpandableListAdapter.java
@@ -0,0 +1,112 @@
+package com.edulectronics.tinycircuit.ui.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.edulectronics.tinycircuit.Models.ExpandedMenu;
+import com.edulectronics.tinycircuit.R;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Wilmer on 1-12-2016.
+ */
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+    private Context context;
+    private List<ExpandedMenu> headers;
+
+    private HashMap<ExpandedMenu, List<String>> children;
+    ExpandableListView expandableListView;
+
+    public ExpandableListAdapter(Context context, List<ExpandedMenu> listHeader,
+                                 HashMap<ExpandedMenu, List<String>> listChildren,
+                                 ExpandableListView view) {
+        this.context = context;
+        this.headers = listHeader;
+        this.children = listChildren;
+        this.expandableListView = view;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this.headers.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        try{
+            return this.children.get(this.headers.get(groupPosition)).size();
+        }catch (NullPointerException e){
+            return 0;
+        }
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this.headers.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return this.children.get(this.headers.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        ExpandedMenu headerTitle = (ExpandedMenu) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.header, null);
+        }
+        TextView textView = (TextView) convertView.findViewById(R.id.submenu);
+        ImageView headerIcon = (ImageView) convertView.findViewById(R.id.iconimage);
+        textView.setText(headerTitle.getIconName());
+        headerIcon.setImageResource(headerTitle.getIconImage());
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        String childText = (String) getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.listsubmenu, null);
+        }
+
+        TextView txtListChild = (TextView) convertView.findViewById(R.id.submenu);
+        txtListChild.setText(childText);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
