548f356ae610fd943eacf1487c0ab3f3252a8493
Bernd van den Hoek <bernd_hoek@hotmail.com>
2016-12-09 14:57:00 +0100

    get Node locations

--- app/src/main/java/com/edulectronics/tinycircuit/Controllers/DrawingController.java
+++ app/src/main/java/com/edulectronics/tinycircuit/Controllers/DrawingController.java
@@ -2,12 +2,37 @@ package com.edulectronics.tinycircuit.Controllers;
 
 import android.graphics.Point;
 
+import com.edulectronics.tinycircuit.Models.Components.Connectors.ConnectionPoint;
+import com.edulectronics.tinycircuit.Models.Components.Connectors.ConnectionPointOrientation;
+
 /**
  * Created by bernd on 09/12/2016.
  */
 
 public class DrawingController {
-    public Point getNodeLocation(int position, int orientation) {
-        return new Point(0, 0);
+
+    private int componentWidth, componentHeight, gridWidth, gridHeight;
+
+    public DrawingController(int componentWidth, int componentHeight, int gridWidth, int gridHeight) {
+        this.componentWidth = componentWidth;
+        this.componentHeight = componentHeight;
+        this.gridWidth = gridWidth;
+        this.gridHeight = gridHeight;
+    }
+
+    public Point getNodeLocation(int position, ConnectionPointOrientation orientation) {
+        return new Point(getXLocation(position, orientation), getYLocation(position, orientation));
+    }
+
+    private int getXLocation(int position, ConnectionPointOrientation orientation) {
+        if (orientation == ConnectionPointOrientation.Left)
+            return (position - 1) * componentWidth;
+        return position * componentWidth;
+    }
+
+    private int getYLocation(int position, ConnectionPointOrientation orientation) {
+        if (orientation == ConnectionPointOrientation.Top)
+            return (position - 1) * componentHeight;
+        return position * componentHeight;
     }
 }
