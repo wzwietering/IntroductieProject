6ad1564c6d81d4dfb02c1bdf13f7dbc3e5422320
Bernd van den Hoek <bernd_hoek@hotmail.com>
2016-12-09 14:39:41 +0100

    Add drawingcontroller

---
+++ app/src/main/java/com/edulectronics/tinycircuit/Controllers/DrawingController.java
@@ -0,0 +1,13 @@
+package com.edulectronics.tinycircuit.Controllers;
+
+import android.graphics.Point;
+
+/**
+ * Created by bernd on 09/12/2016.
+ */
+
+public class DrawingController {
+    public Point getNodeLocation(int position, int orientation) {
+        return new Point(0, 0);
+    }
+}
