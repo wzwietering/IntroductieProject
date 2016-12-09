556051fd641f05c817201e8bb59d9aeb319977ed
Wilmer Zwietering <wzwietering@gmail.com>
2016-12-09 15:17:56 +0100

    DrawView cleanup

--- app/src/main/java/com/edulectronics/tinycircuit/Views/DrawView.java
+++ app/src/main/java/com/edulectronics/tinycircuit/Views/DrawView.java
@@ -2,8 +2,6 @@ package com.edulectronics.tinycircuit.Views;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -12,21 +10,15 @@ import android.view.View;
  */
 
 public class DrawView extends View {
-    Paint paint = new Paint();
-    private int strokeWidth = 8;
     private Canvas canvas;
 
     public DrawView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        paint.setColor(Color.RED);
-        paint.setStrokeWidth(strokeWidth);
     }
 
     @Override
     public void onDraw(Canvas canvas) {
         this.canvas = canvas;
-        canvas.drawLine(0, 0, 300, 300, paint);
-        super.onDraw(canvas);
     }
 
     public Canvas getCanvas(){
