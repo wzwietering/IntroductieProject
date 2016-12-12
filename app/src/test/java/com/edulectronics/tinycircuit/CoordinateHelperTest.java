03915dded2bae22dd66099a143892abce7646b7e
Wilmer Zwietering <wzwietering@gmail.com>
2016-12-12 14:53:59 +0100

    Test class

---
+++ app/src/test/java/com/edulectronics/tinycircuit/CoordinateHelperTest.java
@@ -0,0 +1,14 @@
+package com.edulectronics.tinycircuit;
+
+import org.junit.Test;
+
+/**
+ * Created by Wilmer on 12-12-2016.
+ */
+
+public class CoordinateHelperTest {
+    @Test
+    public void getXTest(){
+
+    }
+}
